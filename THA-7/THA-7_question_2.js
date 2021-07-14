682180b9d1406cd659e9f596e5bb901793eaa5e7
Km Mansi <54065778+Mansi144000@users.noreply.github.com>
2021-07-14 20:12:49 +0530

    Delete THA-7 directory

--- THA-7/THA-7_question_2.js
+++
@@ -1,9 +0,0 @@
-var student =  { 
-    name : "Km Mansi", 
-    year : "3rd year", 
-    rollno : 180291  };
-  console.log(student);
-  delete student.rollno;
-  console.log(student);
-  var objsize = Object.objsize(student);
-console.log('Size of the current object : '+objsize);
